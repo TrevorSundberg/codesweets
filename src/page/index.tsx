9b5cf21cbf5142e4d40510c6eee64471cede9528
Trevor Sundberg <tsundberg@s2.systems>
2019-06-20 09:05:23 -0700

    Using schema from actual component.

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,36 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Form from "react-jsonschema-form";
-
 import {Hello} from "./components/hello";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const GitHub = require("../../bin/tasks/github").default;
 
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
   document.getElementById("example")
 );
 
-const schema = {
-  properties: {
-    done: {
-      default: false,
-      title: "Done?",
-      type: "boolean"
-    },
-    title: {
-      default: "A new task",
-      title: "Title",
-      type: "string"
-    }
-  },
-  required: ["title"],
-  title: "Todo",
-  type: "object"
-};
-
 const log = (type: any) => console.log.bind(console, type);
 
 ReactDOM.render(
-  <Form schema={schema as any}
+  <Form schema={GitHub.GitHubCreateRepository.meta.schema}
     onChange={log("changed")}
     onSubmit={log("submitted")}
     onError={log("errors")} />
