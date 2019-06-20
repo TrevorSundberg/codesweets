8c9c59692466a0616d02e8b18f5b1e937845d368
Trevor Sundberg <tsundberg@s2.systems>
2019-06-20 08:57:30 -0700

    Generated a sample form from a schema (react).

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,9 +1,38 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import Form from "react-jsonschema-form";
 
 import {Hello} from "./components/hello";
 
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
   document.getElementById("example")
+);
+
+const schema = {
+  properties: {
+    done: {
+      default: false,
+      title: "Done?",
+      type: "boolean"
+    },
+    title: {
+      default: "A new task",
+      title: "Title",
+      type: "string"
+    }
+  },
+  required: ["title"],
+  title: "Todo",
+  type: "object"
+};
+
+const log = (type: any) => console.log.bind(console, type);
+
+ReactDOM.render(
+  <Form schema={schema as any}
+    onChange={log("changed")}
+    onSubmit={log("submitted")}
+    onError={log("errors")} />
+  , document.getElementById("app")
 );
