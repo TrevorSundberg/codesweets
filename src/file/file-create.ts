f7106f9ccad5f3bd4a5b8eb9d3289698b04ec329
Trevor Sundberg <tsundberg@s2.systems>
2019-06-22 11:13:29 -0700

    Removed extras from file-create.

--- src/file/file-create.ts
+++ src/file/file-create.ts
@@ -1,34 +1,11 @@
 import {TaskMeta, TaskWithData} from "../sweet/sweet";
 import {Directory} from "./directory";
 
-export declare enum FileFlags {
-  r,
-  "r+",
-  rs,
-  sr,
-  "rs+",
-  "sr+",
-  w,
-  wx,
-  xw,
-  "w+",
-  "wx+",
-  "xw+",
-  a,
-  ax,
-  xa,
-  "a+",
-  "ax+",
-  "xa+"
-}
-
-export type FileCreateEncoding = "utf8" | "ascii" | "base64" | "hex"
+export type FileCreateEncoding = "utf8" | "ascii" | "base64" | "hex";
 
 export interface FileCreateData {
   path: string;
   content: string;
-  flag?: FileFlags;
-  mode?: number;
 
   /** @default utf8 */
   encoding: FileCreateEncoding;
@@ -46,8 +23,7 @@ export class FileCreate extends TaskWithData<FileCreateData> {
     const path = Directory.resolve(this, this.data.path);
     const buffer = Buffer.from(this.data.content, this.data.encoding);
     await this.fs.promises.writeFile(path, buffer, {
-      encoding: "binary",
-      flag: this.data.flag || "w"
+      encoding: "binary"
     });
   }
 }
