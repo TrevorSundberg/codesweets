5c480c557bc371445fb4e8f9a6e0b8dc8ed36dc3
Trevor Sundberg <tsundberg@s2.systems>
2019-06-24 19:00:27 -0700

    Change message to test deployment.

--- src/page/components/hello.tsx
+++ src/page/components/hello.tsx
@@ -8,6 +8,6 @@ export interface HelloProps { compiler: string; framework: string }
  */
 export class Hello extends React.Component<HelloProps, {}> {
   public render () {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    return <h1>Code Sweets is running with {this.props.compiler} and {this.props.framework}!</h1>;
   }
 }
