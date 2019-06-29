2d1d9b77fa38ffd4a266c37d2a2ef019d6c0aa57
Trevor Sundberg <tsundberg@s2.systems>
2019-06-29 16:20:11 -0700

    Output and JSON (layout with grid).

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Col, Glyphicon, Grid, Panel, Row} from "react-bootstrap";
 import Form, {IChangeEvent} from "react-jsonschema-form";
 // eslint-disable-next-line id-length
 import $ from "jquery";
+import {Controlled as CodeMirror} from "react-codemirror2";
 import {Hello} from "./components/hello";
 import {JSONSchema6} from "json-schema";
 
@@ -17,9 +19,6 @@ ReactDOM.render(
 
 // eslint-disable-next-line no-eval
 const loadJS = (url: string): Promise<any> => eval(`import(${JSON.stringify(url)})`);
-
-const log = (type: any) => console.log.bind(console, type);
-
 
 const taskNames: string[] = [];
 const taskSchemas: JSONSchema6[] = [];
@@ -74,17 +73,24 @@ globals.ontaskmeta = (meta: TaskMeta) => {
 const main = async () => {
   const sweet = await loadJS("/bin/tasks/sweet/sweet.js");
 
+  let render: () => void = null;
+  let output = "";
   const submit = (event: IChangeEvent<TaskSaved>) => {
+    output = "";
     const saved = event.formData;
     const task = sweet.default.Task.deserialize(saved) as TaskRoot;
     console.log("root", task);
     task.logger = (component, type, ...args) => {
       console.log(type, ...args);
+      output += `${args.join(", ")}\n`;
+      render();
       if (type === "error") {
-        const query: any = $(`#root_components_${component.id}_typename`);
+        const query = $(`#root_components_${component.id}_typename`);
         query.popover({
-          content: args.join("\n")
-        }).popover("show");
+          content: args.join("\n"),
+          trigger: "focus"
+        });
+        query.focus();
       }
     };
     task.run();
@@ -97,12 +103,69 @@ const main = async () => {
   ]);
 
   console.log(JSON.stringify(schema));
-  ReactDOM.render(
-    <Form schema={schema}
-      onChange={log("changed")}
-      onSubmit={submit}
-      onError={log("errors")} />
-    , document.getElementById("app")
-  );
+  let data = {};
+  let code = "{}";
+  let glyph = "ok";
+
+  const codeChanged = (editor: any, codeData: any, value: string) => {
+    console.log("CODE CHANGED");
+    code = value;
+    try {
+      data = JSON.parse(value);
+      glyph = "ok";
+    } catch (err) {
+      console.log(err);
+      glyph = "remove";
+    }
+    render();
+  };
+
+  const formChanged = (event: IChangeEvent<TaskSaved>) => {
+    console.log("FORM CHANGED");
+    glyph = "ok";
+    data = event.formData;
+    code = JSON.stringify(data, null, 2);
+    render();
+  };
+
+  render = () => {
+    ReactDOM.render(
+      <Grid>
+        <Row>
+          <Col md={7}>
+            <Form
+              schema={schema}
+              formData={data}
+              onChange={formChanged}
+              onSubmit={submit} />
+          </Col>
+          <Col md={5}>
+            <Row>
+              <Panel>
+                <Panel.Heading><Glyphicon glyph={glyph} /> JSON</Panel.Heading>
+                <CodeMirror value={code} onBeforeChange={codeChanged} options={{
+                  lineNumbers: true,
+                  tabSize: 2
+                }} />
+              </Panel>
+            </Row>
+            <Row>
+              <Panel>
+                <Panel.Heading>Output</Panel.Heading>
+                <CodeMirror value={output} onBeforeChange={null} options={{
+                  lineNumbers: true,
+                  readOnly: true,
+                  tabSize: 2
+                }} />
+              </Panel>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+      , document.getElementById("app")
+    );
+  };
+
+  render();
 };
 main();
